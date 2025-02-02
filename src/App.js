35a4488cef47776045b68f719974f2b42d6f9849
azhar <azhar.58@gmail.com>
2025-02-02 20:51:55 +0800

    Removed default template and add intro component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Menu from './Components/Taskbar/Menu.js';
+import About from './Components/About/About.js';
 
 
 function App() {
   return (
     <div className="App">
       <Menu />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          i'm azhar
-        </a>
-      </header>
+      <About />
     </div>
   );
 }
--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,1 +1,13 @@
 import React from "react";
+
+const About = () =>
+<header>
+    <h1>
+        Hi, I'm Azhar.
+    </h1>
+    <h2>
+        Software developer, tech enthusiast, and a lifelong learner.
+    </h2>
+</header>
+
+export default About;
